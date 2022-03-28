d4601241a03ca20354f79fe9328eca976630b393
Muhammad Umair Yaqub <77500484+Umair-Yaqub@users.noreply.github.com>
2022-03-28 12:29:14 +0500

    Up

---
+++ ARK_01.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <conio.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+using namespace std ;
+
+int main (){
+	
+	cout<< "                 A R K                     \n" 
+		<< "                ARCHIVES                   \n\n";
+	
+	cout << "                 "	;
+	
+	for (int i = 0; i < 3; i++) {
+        cout << ". ";
+        cout.flush();
+        sleep(1);
+    } 
+	cout<< "\n.        .  RIFT DEVELOPMENT  .        .   \n";
+	
+	cout << "\n                 ";
+	
+	for (int i = 0; i < 3; i++) {
+        cout << ". ";
+        cout.flush();
+        sleep(1);
+    }
+	cout << "\n               G H O S T\n" << "            P R O T O C O L\n\n" ;
+	cout<< "\n\n[SYSTEM DATABASE]\n" 
+		<< "[ARK.ROOT.STR.001]\n"
+		<< "VER. 1.0005\n";
+	cout << "\n                 ";
+	for (int i = 0; i < 3; i++) {
+        cout << ". ";
+        cout.flush();
+        sleep(1);
+    }
+	cout << "\n         I N I T I A L I Z I N G\n";
+	
+	
+	 
+	cout<< ".        .                    .        .\n\n" ;
+	
+    cout.flush();
+    cout << "\n                 ";
+    for (int i = 0; i < 3; i++) {
+        cout << ". ";
+        cout.flush();
+        sleep(1);
+    }
+		
+	system("start https://youtu.be/46ao3qU-QvQ");
+	
+	return 0;
+
+}
